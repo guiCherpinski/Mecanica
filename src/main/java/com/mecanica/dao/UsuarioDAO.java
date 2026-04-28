41169d22b128a61d5be4af2bf0eb8030f3588225
guiCherpinski <cherpinskiguilherme@gmail.com>
2026-04-28 18:19:40 -0300

    feat: Salva progresso na UsuarioDAO

--- src/main/java/com/mecanica/dao/UsuarioDAO.java
+++ src/main/java/com/mecanica/dao/UsuarioDAO.java
@@ -2,9 +2,15 @@ package com.mecanica.dao;
 
 import com.mecanica.model.Usuario;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class UsuarioDAO {
 
     public void insertUsuario(Usuario usuario){
-
+        String sql = "INSERT INTO usuario (nome_usuario, email_usuario, senha_usuario, cpf_usuario, telefone_usuario," +
+                "perfil_usuario, ativo_usuario, logradouro_usuario, numero_usuario, complemento_usuario, bairro_usuario," +
+                "cidade_usuario, estado_usuario, cep_usuario, especialidade_usuario, categoria_usuario) values (?,?,?," +
+                "?,?,?,?,?,?,?,?,?,?,?,?,?)";
     }
 }
